1db81ce0bc9761e4a14eaa827a05867c3b3319d5
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-26 23:23:32 +0530

    Common Files

---
+++ Common File/Learning Java/src/Simple.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Simple {
+    public static void main(String[] args) {
+        int a, b, c;
+        System.out.println("Enter Value for B & C:");
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
