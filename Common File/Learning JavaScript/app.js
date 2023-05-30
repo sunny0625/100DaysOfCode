b2819d99f856ab288a906b8b3cbff65eed72117d
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-30 07:05:23 +0530

    Arithmetic Operation

---
+++ Common File/Learning JavaScript/app.js
@@ -0,0 +1,17 @@
+//var x = "Hello World";
+//console.log(x);
+//
+//var y = 999;
+//console.log(y);
+//
+//var z = true;
+//console.log(z);
+//
+//console.log(typeof(z));
+
+//////////////////Arithmetic Operators
+
+var x = 90;
+var y = 10;
+console.log(x + y);
+console.log(x % y);
