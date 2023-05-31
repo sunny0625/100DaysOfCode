15774983480412ae45786318eb9b916000f383e8
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-31 08:35:41 +0530

    Logical operators

--- Common File/Learning JavaScript/app.js
+++ Common File/Learning JavaScript/app.js
@@ -11,7 +11,44 @@
 
 //////////////////Arithmetic Operators
 
-var x = 90;
+//var x = 90;
+//var y = 10;
+//console.log(x + y);
+//console.log(x % y);
+
+/////////////Logical And Ternary
+
+var x = 20;
 var y = 10;
-console.log(x + y);
-console.log(x % y);
+var z = x > y;
+
+console.log(x > y);
+console.log(z);
+console.log(typeof(z));
+
+var a = 40;
+var b = 20;
+var c = a < b;
+
+console.log(typeof(c));
+console.log(c);
+
+// &&
+console.log(z && c);
+
+// ||
+console.log(z || c);
+
+// !
+console.log(!c);
+
+var age = 19;
+var eligiblevote = (age > 18) ? "PERSON CAN VOTE" : "PERSON IS UNDER 18";
+
+console.log(eligiblevote);
+
+console.log(z &= c);
+
+console.log(z |= c);
+
+console.log(z != c);
