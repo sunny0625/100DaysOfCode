916bf61c4632f9f6fcb5b226c11641a93b3031df
Sukhbir Singh <sukhbir2506@outlook.com>
2023-06-02 12:52:08 +0530

    Operators

--- Common File/Learning JavaScript/app.js
+++ Common File/Learning JavaScript/app.js
@@ -18,37 +18,88 @@
 
 /////////////Logical And Ternary
 
-var x = 20;
-var y = 10;
-var z = x > y;
+//var x = 20;
+//var y = 10;
+//var z = x > y;
+//
+//console.log(x > y);
+//console.log(z);
+//console.log(typeof(z));
+//
+//var a = 40;
+//var b = 20;
+//var c = a < b;
+//
+//console.log(typeof(c));
+//console.log(c);
+//
+//// &&
+//console.log(z && c);
+//
+//// ||
+//console.log(z || c);
+//
+//// !
+//console.log(!c);
+//
+//var age = 19;
+//var eligiblevote = (age > 18) ? "PERSON CAN VOTE" : "PERSON IS UNDER 18";
+//
+//console.log(eligiblevote);
+//
+//console.log(z &= c);
+//
+//console.log(z |= c);
+//
+//console.log(z != c);
 
-console.log(x > y);
-console.log(z);
-console.log(typeof(z));
+//var a = "10";
+//var b = 10;
+//
+//console.log(a);
+//console.log(b);
+//
+//console.log(typeof(a));
+//console.log(typeof(b));
+//
+//console.log(a == b);
+//console.log(a === b);
+//
+//var c;
+//var d = null;
 
-var a = 40;
-var b = 20;
-var c = a < b;
+//console.log(typeof(c));
+//console.log(typeof(d));
 
-console.log(typeof(c));
+// undefined: when an variable is only declared and not defined then it is undefined
+
+// null: null is just same as the other string, number etc
+
+// =
+
+// == this operator doesnt care about the type of the value. It just compares the two values.
+
+// === this operator cares even for the values and the types of it.
+
+//////Concatination////////
+
+var a = 10;
+var b = 30;
+
+var d = "Sunny";
+var e = "Singh";
+
+var f = d + e;
+var c = a + b;
 console.log(c);
+console.log(f);
 
-// &&
-console.log(z && c);
+////////Type Conversion/////////////
 
-// ||
-console.log(z || c);
+var g = "10";
+var h = 20;
+var j = 30;
+var i = j + g + h;
 
-// !
-console.log(!c);
-
-var age = 19;
-var eligiblevote = (age > 18) ? "PERSON CAN VOTE" : "PERSON IS UNDER 18";
-
-console.log(eligiblevote);
-
-console.log(z &= c);
-
-console.log(z |= c);
-
-console.log(z != c);
+console.log(i);
+console.log(typeof(i));
